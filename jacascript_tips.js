ceace78267aa83f1476dfb2cb87df1042544e5d8
Matrix <jakoo@MatrixdeMacBook-Pro-2.local>
2018-10-30 03:00:05 +0800

    init

---
+++ jacascript_tips.js
@@ -0,0 +1,58 @@
+//1
+const foo = {name: ’tom’, age:30, nervous:false };
+const bar = {name: ’dick’, age:40, nervous:false };
+const baz = {name: ’harry’, age:50, nervous:true };
+
+
+//shit code 💩
+console.log(foo);
+
+
+//good code 😀
+
+console.log(“%c My Friends”, “color : orange; front-weight: bold")
+console.log({foo, bar, baz})
+//
+console.table([foo,bar,baz])
+
+//2
+console.time(“looper”);
+
+while(i<10000){i++}
+
+console.timeEnd(“looper”)
+
+//3 重要的方法例如删除，不被意外的执行两次
+
+ const deleteMe = () => console.trace(‘bye bye database’);
+
+ deleteMe()
+ deleteMe()
+
+
+//4 destructuring.js
+const turtle = {
+    name: ‘Bob’,
+    meal: 10,
+    diet: ‘berries'
+    //...
+}
+
+//shit code 💩
+
+function feed(animal) {
+    return `Feed` ${animal.name} ${animal.meal} kilos of ${animal.diet};
+}
+
+//good code 😀
+
+function feed({name,meal,diet}) {
+    return `Feed` ${name} ${meal} kilos of ${diet};
+}
+//OR
+
+function feed(animal) {
+    const {name,meal, diet} = animal
+    return `Feed` ${name} ${meal} kilos of ${diet};
+}
+
