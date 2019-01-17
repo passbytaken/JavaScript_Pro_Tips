235fefe1935a90d809f6fd1c8ac22629daba7f3b
Matrix <jakoo@MatrixdeMacBook-Pro-2.local>
2019-01-17 14:14:31 +0800

    loops

--- loops.js
+++ loops.js
@@ -22,3 +22,26 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.timeEnd('h');
+
+const equine = {horse: '🐴', cat: '🐱', unicorn: '🦄'}
+
+for (const key of equine) {
+    console.log(equine[key]);
+}
+
+for (const v of Object.values(equine)) {
+    console.log(v);
+}
+
+const equine1 = new Map(
+    Object.entries({horse: '🐴', cat: '🐱', unicorn: '🦄'})
+) 
+
+for (const v of equine1.values()) {
+    console.log(v);
+}
+
+const faces = ['😆', '😔', '💰', '😁', '😓']
+
+const faces = faces.filter(v => v === '💰')
+console.log(faces); //['💰']
